60d3e775a01ae2be848965a001b3f74ebc99b200
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-09-17 22:06:10 +0300

    code fix

--- src/api/search-books-api.ts
+++ src/api/search-books-api.ts
@@ -5,16 +5,14 @@ const instance = axios.create({
     baseURL: "https://www.googleapis.com/books/v1/volumes",
 })
 
-const API_KEY = "AIzaSyB6umoA_pBqpX2zzOCnS4CBgAPBSL-S2ck";
-
 export const searchBooksApi = {
    getBooks(searchSubstring: string, index: number, order: string, filter: string) {
        const category = filter === "all" ? "" : filter
 
-       return instance.get<ResponseType>(`?q=${searchSubstring}+subject:${category}&maxResults=${MAX_RESULT}&startIndex=${index * MAX_RESULT}&orderBy=${order}&key=${API_KEY}`)
+       return instance.get<ResponseType>(`?q=${searchSubstring}+subject:${category}&maxResults=${MAX_RESULT}&startIndex=${index * MAX_RESULT}&orderBy=${order}&key=${process.env.REACT_APP_API_KEY}`)
    },
     getDetailInfoBook(id: string) {
-       return instance.get<SearchBook>(`/${id}?key=${API_KEY}`)
+       return instance.get<SearchBook>(`/${id}?key=${process.env.REACT_APP_API_KEY}`)
     }
 }
 
